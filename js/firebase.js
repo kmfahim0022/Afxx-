fe9744bb1767ee17fc035e1a6dce0bdf7737ec03
kmfahim0022 <kmfahim172@gmail.com>
2026-07-20 02:25:30 +0600

    Add Firebase configuration file

---
+++ js/firebase.js
@@ -0,0 +1,35 @@
+{
+  "project_info": {
+    "project_number": "1089769201854",
+    "firebase_url": "https://d-app-8e117-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "project_id": "d-app-8e117",
+    "storage_bucket": "d-app-8e117.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:1089769201854:android:aac6f2b0e40a61c24b7eef",
+        "android_client_info": {
+          "package_name": "com.secure.app"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAIA_Ct9kfPN16hr-aoLc4fajvGsYHOuMs"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": []
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
+
+
+
+
+
